c2951172211ff9d25f3bdb0c8a79643dd73ddedf
Nakano <87232888+ninesss1@users.noreply.github.com>
2025-08-11 10:17:24 +0700

    ก

--- my-app/src/components/Home_Page.jsx
+++ my-app/src/components/Home_Page.jsx
@@ -1,26 +1,42 @@
+import React, { useEffect, useRef } from "react";
 import "./css/Home_Page.css";
-import React from "react";
 
+import cat1 from "../assets/cat_1.png";
+import cat2 from "../assets/cat_2.png";
+import dog1 from "../assets/dog_1.png";
+import dog2 from "../assets/dog_2.png";
 
 function Home_Page() {
-    // อยากให้เวลามาหน้านี้จะทำการรีเฟรชหน้า แค่ครั้งเดียว
-    // เพื่อให้แน่ใจว่าไม่มีข้อมูลเก่าค้างอยู่
-    // eslint-disable-next-line react-hooks/rules-of-hooks, no-undef
-    React.useEffect(() => {
-    const hasReloaded = sessionStorage.getItem('hasReloaded');
-  
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const hasReloaded = sessionStorage.getItem("hasReloaded");
     if (!hasReloaded) {
-      sessionStorage.setItem('hasReloaded', 'true');
+      sessionStorage.setItem("hasReloaded", "true");
       window.location.reload();
     }
   }, []);
+
+  useEffect(() => {
+    const images = containerRef.current.querySelectorAll(".cat-image");
+    images.forEach((image, index) => {
+      setTimeout(() => {
+        image.classList.add("visible");
+      }, index * 300);
+    });
+  }, []);
+
   return (
-    <>
-     
-    </>
+    <section className="home">
+      <div className="home-container" ref={containerRef}>
+        <img src={cat1} alt="Cat_1" className="cat-image" />
+        <img src={cat2} alt="Cat_2" className="cat-image" />
+        <img src={dog1} alt="Dog_1" className="cat-image" />
+        <img src={dog2} alt="Dog_2" className="cat-image" />
+      </div>
+    </section>
   );
 }
 
 export default Home_Page;
-
 
